3c3ccf98fe77957660d8b87fe9743d65191f4ef4
Aljawharah Alhammad <105801508+Aljawharahs@users.noreply.github.com>
2024-04-07 00:48:53 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import ViewHashedVoice from './Components/ViewHashedVoice';
+
+function App() {
+  return(
+   <div >
+    <ViewHashedVoice/>
+  </div>
+);
+}
+export default App;
