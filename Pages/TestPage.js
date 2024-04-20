12175df70074fb5198a730fae65256ef5fba312b
SabaAlhoshan <105789612+SabaAlhoshan@users.noreply.github.com>
2024-04-20 22:20:52 +0300

    Add files via upload

---
+++ Pages/TestPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+
+function TestPage() {
+
+    return(
+    
+    
+    
+    <div> 
+    
+
+    <div> بسم الله lets start </div>
+    
+    </div>
+    
+    )
+    }
+    
+    export default TestPage
---
+++ Pages/ViewWalletPage.js
@@ -0,0 +1,98 @@
+
+
+
+
+
+
+import React from "react";
+
+
+import logo from './logo.svg';
+import './FingerprintPage.css';
+import logo1 from './vmLOGO.png';
+import profile1 from'./profile.png';
+import SoundWave from './soundwave1.png';
+import wallet from './wallet.png';
+
+///////////
+import { Link } from "react-router-dom";
+
+import { useNavigate } from 'react-router-dom';
+
+
+function ViewWalletPage() {
+    const navigate = useNavigate();
+return(
+
+
+<div className="App">
+      <header className="App-header">
+      <header className="App-header2">
+        <img src={logo1} className="App-logo" alt="logo" />
+        </header>
+        <header className="App-header1">
+        <p>
+        Protect it,
+ <br />
+ Empower it
+<br />
+Own it..
+        </p>
+        </header>
+        <header className="App-header3">
+       
+        </header>
+        <header className="App-header4">
+  
+
+
+  
+</header>
+<header>
+<div >
+ 
+
+
+
+
+{/*هذي طريقة النافبار الاولى باستخدام لنك */} 
+{/*  <button > 
+  <Link to="/Fingerprint"> cliclk meee </Link>
+  </button> */}
+
+
+
+
+
+
+
+
+</div>
+</header>
+
+
+<button className="button1"   on onClick={() => navigate('/About')}>about us </button>
+        <button className="button2" on onClick={() => navigate('/Test')} >why virtual me </button>
+        <button className="button3" on onClick={() => navigate('/Homee')}>home </button>
+        <button className="button4" on onClick={() => navigate('/Agreements')}>agreements </button>
+        <button class="button5" on onClick={() => navigate('/Profile')}>
+  <img src={profile1} alt="profile" />
+</button>
+      
+        <p>
+
+  <div class="line"></div>
+  
+</p>
+
+
+      </header>
+
+
+      
+    </div>
+
+)
+}
+
+export default ViewWalletPage
