4fe9a61016a368bb208adea25e13dda797cbf8b6
Reema Alshehri <157634904+reemamush@users.noreply.github.com>
2024-04-27 21:05:55 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
+// import Main from './main.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +12,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
